2aafb29453beccefc4357a75b07b40e8c4a2d733
Zubovich Vladimir <moraviin@gmail.com>
2018-10-28 21:15:59 +0300

    style: == => ===

--- projects/make.js
+++ projects/make.js
@@ -2,7 +2,7 @@ function make(init) {
   let arr = [];
   arr = arr.concat(init);
   function f(...args) {
-    if (typeof args[0] == 'function') {
+    if (typeof args[0] === 'function') {
       return arr.reduce(args[0]);
     }
     arr = arr.concat(args);
--- projects/sum_of_other.js
+++ projects/sum_of_other.js
@@ -2,7 +2,7 @@ function sumOfOther(arr) {
   return arr.map((elem, i, arr) => {
     return (
       arr.reduce((acc, val) => {
-        return (acc += val);
+        return (acc + val);
       }, 0) - elem
     );
   });
